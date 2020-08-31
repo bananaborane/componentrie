6b96971f1ea62750040244eaa314dde0fa90f26a
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-08-30 22:06:58 -0700

    installed axios and updated package.json

--- componentrie-client/src/pages/home.js
+++ componentrie-client/src/pages/home.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Grid from '@material-ui/core/Grid'
+import axios from 'axios'
 
 export default function home() {
+    useEffect(() => {
+
+    }, [])
     return (
-        <Grid container>
+        <Grid container spacing={16}>
             <Grid item sm={8} xs={12}>
                 <p>Content here</p>
             </Grid>
