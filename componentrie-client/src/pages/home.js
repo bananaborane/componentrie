2d4f892a81957538e70996030b2c5fe66df362e8
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-08-30 21:55:05 -0700

    implementing grid from material ui in home page

--- componentrie-client/src/pages/home.js
+++ componentrie-client/src/pages/home.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Grid from '@material-ui/core/Grid'
 
 export default function home() {
     return (
-        <div>
-            <h2>Home page</h2>
-        </div>
+        <Grid container>
+            <Grid item sm={8} xs={12}>
+                <p>Content here</p>
+            </Grid>
+            <Grid item sm={8} xs={12}>
+                <p>Profile here</p>
+            </Grid>
+        </Grid>
     )
 }
