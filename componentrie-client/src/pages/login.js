e774d041fa09cc16837366c3fff03c72fd5c6a16
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-09-11 11:56:11 -0700

    started on login form and styling

--- componentrie-client/src/pages/login.js
+++ componentrie-client/src/pages/login.js
@@ -1,16 +1,42 @@
 import React from 'react'
 import withStyles from '@material-ui/core/styles/withStyles'
 
-const styles = {
+// MUI imports
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
+import TextField from '@material-ui/core/TextField'
 
+
+const styles = {
+    form: {
+        textAlign: 'center'
+    } 
 }
 
 function login(props) {
+
     const { classes } = props
+
+    const handleSubmit = function(event){
+        console.log('from handleSubmit')
+    }
+
     return (
-        <div>
-            <h2>Login page</h2>
-        </div>
+        <Grid container className={classes.form}>
+            <Grid item sm/>
+            <Grid item sm/>
+                <p>hi mom</p>
+                <Typography variant='h3' className={classes.pageTitle}>
+                    Login
+                </Typography>
+                <form noValidate onSubmit={handleSubmit}>
+                <TextField id='email' name='email' type='email' label='Email' className={classes.textField}>
+                    
+                </TextField>
+                </form>
+            </Grid>
+            <Grid item sm/>
+        </Grid>
     )
 }
 
