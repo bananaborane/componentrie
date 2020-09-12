dd339237c23f2a32bac1d7e789d7b8a808b650bd
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-09-12 15:52:40 -0700

    added fields and changed login state properties

--- componentrie-client/src/pages/login.js
+++ componentrie-client/src/pages/login.js
@@ -5,6 +5,7 @@ import withStyles from '@material-ui/core/styles/withStyles'
 import Grid from '@material-ui/core/Grid'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField'
+import Button from '@material-ui/core/Button'
 
 
 const styles = {
@@ -13,13 +14,21 @@ const styles = {
     } 
 }
 
-    const [email, setEmail] = useState('')
-    const [password, setPassword] = useState('')
-    const [loading, setLoading] = useState(false)
 
 function login(props) {
-
+    
     const { classes } = props
+        const [loginProperties, setLoginProperties] = useState({
+            email: '',
+            password: '',
+            loading: false,
+            errrors: {},
+        })
+
+    const handleChange = event => {
+        setLoginProperties({ ...loginProperties, [event.target.name]: event.target.value })
+    }
+
 
 
 
@@ -36,9 +45,9 @@ function login(props) {
                     Login
                 </Typography>
                 <form noValidate onSubmit={handleSubmit}>
-                <TextField id='email' name='email' type='email' label='Email' className={classes.textField}>
-                    
-                </TextField>
+                    <TextField id='email' name='email' type='email' label='Email' className={classes.textField} value={email} onChange={handleChange} fullWidth />
+                    <TextField id='password' name='password' type='password' label='Password' className={classes.textField} value={password} onChange={handleChange} fullWidth />
+                    <Button type='submit' variant='contained' color='primary' className={classes.button} />
                 </form>
             </Grid>
             
