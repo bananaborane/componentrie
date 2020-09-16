4f80945248d99f30ccf384357adfed33ed90d431
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-09-16 15:48:13 -0700

    redux store implemented into app

--- componentrie-client/src/App.js
+++ componentrie-client/src/App.js
@@ -7,6 +7,10 @@ import { BrowerRouter as Router, Route, Switch } from 'react-router-dom'
 import themeFile from './util/theme'
 import jwtDecode from 'jwt-decode'
 
+// Redux
+import { Provider } from 'react-redux';
+import store from './redux/store'
+
 //Components
 import Navbar from './components/Navbar'
 import AuthRoute from './util/AuthRoute'
@@ -34,18 +38,20 @@ if (token){
 function App() {
   return (
     <MuiThemeProvider theme={theme}>
-      <div className="App">
-        <Router>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={home} />
-              <AuthRoute path="/login" component={login} authenticated={authenticated} />
-              <AuthRoute path="/signup" component={signup} authenticated={authenticated} />
-            </Switch>
-          </div>
-        </Router>
-      </div>
+      <Provider store={store}>
+        <div className="App">
+          <Router>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={home} />
+                <AuthRoute path="/login" component={login} authenticated={authenticated} />
+                <AuthRoute path="/signup" component={signup} authenticated={authenticated} />
+              </Switch>
+            </div>
+          </Router>
+        </div>
+      </Provider>
     </MuiThemeProvider>
   );
 }
