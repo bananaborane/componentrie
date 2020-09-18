10d172c71293955f31b3e5e55a66a2c812ac40a8
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-09-18 09:30:07 -0700

    updated user actions with helper function and logout action

--- componentrie-client/src/redux/actions/userActions.js
+++ componentrie-client/src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_ERRORS } from "../types";
+import { SET_ERRORS, SET_UNAUTHENTICATED } from "../types";
 import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types'
 
 import axios from 'axios'
@@ -8,9 +8,7 @@ export const loginUser = (userData, history) => dispatch => {
     axios.post('/login', userData)
     .then(res => {
         console.log(res.data)
-        const FBIdToken = `Bearer ${res.data.token}`
-        localStorage.setItem('FBIdToken', FBIdToken)
-        axios.defaults.headers.common['Authorization'] = FBIdToken;
+        setAuthorizationHeader(res.data.token)
         setLoginProperties({ ...loginProperties, loading: false })
         dispatch(getUserData())
         dispatch({ type: CLEAR_ERRORS })
@@ -24,6 +22,32 @@ export const loginUser = (userData, history) => dispatch => {
     })
 }
 
+
+export const signupUser = (newUserData, history) => dispatch => {
+    dispatch({ type: LOADING_UI });
+    axios.post('/signup', newUserData)
+    .then(res => {
+        console.log(res.data)
+        setAuthorizationHeader(res.data.token)
+        setLoginProperties({ ...loginProperties, loading: false })
+        dispatch(getUserData())
+        dispatch({ type: CLEAR_ERRORS })
+        history.push('/')
+    }) 
+    .catch(err => {
+        dispatch({
+            type: SET_ERRORS,
+            payload: err.response.data
+        })
+    })
+}
+
+export const logoutUser = () => (dispatch) => {
+    localStorage.removeItem('FBIdToken');
+    delete axios.defaults.headers.common['Authorization'];
+    dispatch({ type: SET_UNAUTHENTICATED })
+}
+
 export const getUserData = () => dispatch => {
     axios.get('/user')
         .then(res => {
@@ -35,4 +59,10 @@ export const getUserData = () => dispatch => {
         .catch(err => {
             console.log(err)
         })
+}
+
+const setAuthorizationHeader = token => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem('FBIdToken', FBIdToken);
+    axios.defaults.headers.common['Authorization'] = FBIdToken;
 }
