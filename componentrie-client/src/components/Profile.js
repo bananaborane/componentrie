bdccf9fe2cedcee228721fccbe53afe9c4456867
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-09-21 15:07:38 -0700

    connected profile.js to global states

--- componentrie-client/src/components/Profile.js
+++ componentrie-client/src/components/Profile.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 // Material UI imports
 import withStyles from '@material-ui/core/styles/withStyles'
 import Button from '@material-ui/core/Button'
 
 
-export default function Profile() {
+function Profile() {
     return (
         <div>
             From Profile.js
         </div>
     )
 }
+
+const mapStateToProps = state => {
+    user: state.user
+}
+
+export default connect(mapStateToProps)(withStyles(styles)(Profile))
