fb0f93e86781fb7297ec07ad554f5e2ece8da108
Lorenzo Guillermo <fdsa4321jkl@yahoo.com>
2020-09-22 12:12:46 -0700

    profile markup ternary completed

--- componentrie-client/src/components/Profile.js
+++ componentrie-client/src/components/Profile.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import dayjs from 'dayjs'
 
 // Material UI imports
 import withStyles from '@material-ui/core/styles/withStyles'
 import Button from '@material-ui/core/Button'
+import Paper from '@material-ui/core/Paper'
 import MuiLink from '@material-ui/core/Link'
 import Typography from '@material-ui/core/Typography'
 
@@ -65,7 +67,7 @@ const styles = {
 
 
 function Profile(props) {
-    const { classes, user: { credentials: { handle, createdAt, imageUrl, bio, website, location }, loading } } = props;
+    const { classes, user: { credentials: { handle, createdAt, imageUrl, bio, website, location }, loading, authenticated } } = props;
     let profileMarkUp = !loading ? (authenticated ? (
         <Paper className={classes.paper}>
             <div className={classes.profile}>
@@ -89,12 +91,24 @@ function Profile(props) {
                         <a href={website} target='_blank' rel='noopener noreferrer'>
                             {'  '}{website}
                         </a>
-                        <hr />
+                        <hr />  
                     </>)}
+                    <CalendarToday color='primary' />{'  '}
+                    <span>Joined {dayjs(createdAt).format('MMM YYYY')}</span>
                 </div>
             </div>
         </Paper>
-    ) : ()) : (<p>Loading...</p>)
+    ) : (
+      <Paper className={classes.paper}>
+        <Typography variant='body2' align='center'>
+          No profile found, please login again
+        </Typography>
+        <div className={classes.buttons}>
+          <Button variant='contained' color='primary' component={Link} to='/login'>login</Button>
+          <Button variant='contained' color='secondary' component={Link} to='/signup'>signup</Button>
+        </div>
+      </Paper>
+    )) : (<p>Loading...</p>)
     return (
         profileMarkUp
     )
