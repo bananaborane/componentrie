7336caa4b134927bee2be43d67584759a2e08f84
Louie <46361373+bananaborane@users.noreply.github.com>
2020-11-27 09:12:34 -0800

    like butotn comp imports and ternary

--- componentrie-client/src/components/LikeButton.js
+++ componentrie-client/src/components/LikeButton.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import MyButton from '../util/MyButton'
 import { Link } from 'react-router-dom'
+
+// Icon Imports
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import FavoriteBorder from '@material-ui/icons/FavoriteBorder'
  
+
+// Redux Imports
+import { connect } from 'react-redux'
 
 
 function LikeButton() {
 
-    const likeButton = !authenticated ? (<MyButton>
-        
+    const likeButton = !authenticated ? (<MyButton tip="Like">
+        <Link to="/login">
+            <FavoriteBorder color='primary' />
+        </Link>
     </MyButton>) : (<MyButton>
-
+        
     </MyButton>)
 
     return (
